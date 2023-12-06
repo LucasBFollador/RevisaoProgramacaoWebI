228c34e5f1abf1b2cacec9ce2640c515e6f36c8f
Lucas Breve Follador <102836317+LucasBFollador@users.noreply.github.com>
2023-12-06 19:21:00 -0300

    Update auth.php

--- Prova/Model/auth.php
+++ Prova/Model/auth.php
@@ -1,48 +1,16 @@
 <?php
+class Autenticacao {
+    public function autenticar($username, $password) {
+        // Credenciais fixas (substitua com valores desejados)
+        $usuarioArmazenado = $username;
+        $senhaArmazenada = $password;
 
-require_once 'config.php';
-
-class Cliente {
-    private $conexao;
-
-    public function __construct($conexao) {
-        $this->conexao = $conexao;
-    }
-
-    public function cadastrar($nome, $sobrenome, $dataNascimento, $email) {
-        $stmt = $this->conexao->conectar()->prepare("INSERT INTO clientes (nome, sobrenome, data_nascimento, email) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $nome, $sobrenome, $dataNascimento, $email);
-        $resultado = $stmt->execute();
-        $stmt->close();
-
-        return $resultado;
-    }
-
-    public function listarClientes() {
-        $query = "SELECT id, nome, sobrenome, data_nascimento, email FROM clientes";
-        $resultado = $this->conexao->conectar()->query($query);
-
-        $clientes = array();
-
-        while ($cliente = $resultado->fetch_assoc()) {
-            $clientes[] = $cliente;
+        // Verificar se as credenciais são válidas
+        if ($username === $usuarioArmazenado) {
+            return true;
+        } else {
+            return false;
         }
-
-        return $clientes;
     }
-
-    public function buscarClientesPorNome($nome) {
-        // Ajuste a consulta SQL conforme necessário
-        $stmt = $this->conexao->conectar()->prepare("SELECT * FROM clientes WHERE nome = ?");
-        $stmt->bind_param("s", $nome);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $clientes = $result->fetch_all(MYSQLI_ASSOC);
-        $stmt->close();
-    
-        return $clientes;
-    }
-    
 }
-
 ?>
